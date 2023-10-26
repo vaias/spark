4159f277b7396dd1525c9fff667e5e9f50680167
Istiak-Ahmed78 <aistiak2078@gmail.com>
2023-10-26 23:51:32 +0600

    f

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:prozapoti/view/intro_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
